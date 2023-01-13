9b3a3c73806bf8e1d384ed1f0e1683879daa9262
Daniil Davidovich <dd.jopgm@gmail.com>
2023-01-14 02:06:46 +0300

    add tabBarController

--- Home Work 14/Sources/Delegates/SceneDelegate.swift
+++ Home Work 14/Sources/Delegates/SceneDelegate.swift
@@ -11,7 +11,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
-    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
@@ -37,11 +36,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 }
 
-//extension SceneDelegate {
-//    static var shared: SceneDelegate {
-//        return (UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate)
-//    }
-//}
 
 
 
